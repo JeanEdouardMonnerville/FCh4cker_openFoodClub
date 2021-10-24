ff1a292235938777b672f8d2a39f16155253e5da
SarahGros <sarah.gros@etud.univ-jfc.fr>
2021-10-24 22:05:37 +0200

    implement HashMap customers et HashMap products + add 2 products and 1 customer

--- src/main/java/cat/tecnocampus/courseproject/WebApplication.java
+++ src/main/java/cat/tecnocampus/courseproject/WebApplication.java
@@ -11,5 +11,16 @@ public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-    
+
+    public HashMap<String, Product> products = new HashMap<String>();
+
+    public HashMap<String, Customer> customers = new HashMap<String>();
+
+    var customer = new Customer(id: "1", name: "Pepe", secondName: "Gimenez",
+    email:"pgimenez@tecnocampus.cat", password:"12345678", role:"");
+    var product = new Product(id_product:"1", name:"peer", category:"fruit", price:"2",
+            measure:"1 kilo", suppliers: "Juan", client_tax:"none", image:"imageNotFound");
+    var product = new Product(id_product:"2", name:"tomato", category:"legume", price:"1",
+    measure:"1 kilo", suppliers: "Juan", client_tax:"none", image:"imageNotFound");
+
 }
