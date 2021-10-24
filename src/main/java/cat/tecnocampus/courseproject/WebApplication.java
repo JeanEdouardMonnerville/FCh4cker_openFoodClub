45721bc7e708de3c0cef93681faf4d334f6a348d
SarahGros <sarah.gros@etud.univ-jfc.fr>
2021-10-24 22:20:54 +0200

    Correction last push

--- src/main/java/cat/tecnocampus/courseproject/WebApplication.java
+++ src/main/java/cat/tecnocampus/courseproject/WebApplication.java
@@ -17,10 +17,10 @@ public class WebApplication {
     public HashMap<String, Customer> customers = new HashMap<String>();
 
     var customer = new Customer(id: "1", name: "Pepe", secondName: "Gimenez",
-    email:"pgimenez@tecnocampus.cat", password:"12345678", role:"");
+    email:"pgimenez@tecnocampus.cat", password:"12345678", role:"admin");
     var product = new Product(id_product:"1", name:"peer", category:"fruit", price:"2",
-            measure:"1 kilo", suppliers: "Juan", client_tax:"none", image:"imageNotFound");
+            measure:"kilo", suppliers: "Juan", client_tax:"0,42", image:"imageNotFound");
     var product = new Product(id_product:"2", name:"tomato", category:"legume", price:"1",
-    measure:"1 kilo", suppliers: "Juan", client_tax:"none", image:"imageNotFound");
+    measure:"kilo", suppliers: "Juan", client_tax:"0,21", image:"imageNotFound");
 
 }
