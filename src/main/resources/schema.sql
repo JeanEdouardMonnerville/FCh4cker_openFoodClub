8178145915a00d5e20a50350e8b7b7077bb686a2
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2021-11-10 12:41:21 +0100

    Product entity and data created

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,6 +8,23 @@ Create table Customer (
     --enabled TINYINT NOT NULL DEFAULT 1
 );
 
+DROP TABLE if exists product;
+CREATE TABLE product
+(
+    id VARCHAR (256) PRIMARY KEY ,
+    name VARCHAR (256) ,
+    price NUMERIC,
+    measure_unit VARCHAR (256),
+    provider VARCHAR (256),
+    vat_type VARCHAR (100),
+    category VARCHAR(256),
+    initial_date DATE,
+    day_of_week VARCHAR(20),
+    num_of_periods INT,
+    period VARCHAR(20),
+    image CLOB
+);
+
 DROP TABLE Subscription;
 Create table Subscription (
     quantity Number(255),
@@ -15,4 +32,5 @@ Create table Subscription (
     product Varchar(255),
     customer Varchar(256),
     CONSTRAINT fk_sub_constumer FOREIGN KEY (customer) REFERENCES customer (id) 
+    CONSTRAINT fk_prod_constumer FOREIGN KEY (product) REFERENCES product (id)
 );
